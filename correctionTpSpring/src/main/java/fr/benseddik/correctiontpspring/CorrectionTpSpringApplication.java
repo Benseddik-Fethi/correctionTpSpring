45741e9bb34371b4ab234e6846bed406cf110c8b
fethi benseddik <fbenseddik@insy2s.fr>
2025-01-24 08:08:20 +0100

    initialisation projet

---
+++ correctionTpSpring/src/main/java/fr/benseddik/correctiontpspring/CorrectionTpSpringApplication.java
@@ -0,0 +1,13 @@
+package fr.benseddik.correctiontpspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorrectionTpSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorrectionTpSpringApplication.class, args);
+    }
+
+}
